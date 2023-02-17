1620aa65fea82792336b4715df8224f4e741ad11
mavibulut0106@gmail.com <mavibulut0106@gmail.com>
2023-02-17 16:07:12 +0300

    Core Katmanı oluşturuldu,

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Abstract
+namespace Core.DataAccess.EntityFramework
 {
-    public interface IEntity
+    internal class EfEntityRepositoryBase
     {
-
     }
 }
--- DataAccess/Concrete/EntityFramework/RestaurantContext.cs
+++ DataAccess/Concrete/EntityFramework/RestaurantContext.cs
@@ -12,7 +12,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=YAGMUR;Database=Restaurant;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server=YAGMUR\SQLEXPRESS;Database=Restaurant;Trusted_Connection=true");
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
