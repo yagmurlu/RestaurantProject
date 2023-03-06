4a80085459ee967d2b24e199e2a14d706fe6fdca
mavibulut0106@gmail.com <mavibulut0106@gmail.com>
2023-03-06 23:37:56 +0300

    WepAPI Created

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return new List<Product>
+            {
+                new Product { ProductId = 3, ProductName = "Ezogelin" },
+                new Product { ProductId = 4, ProductName = "Mercimek" }
+            };
+        }
+    }
+}
