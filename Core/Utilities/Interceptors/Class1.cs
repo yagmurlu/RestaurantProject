d33910dace09661988dd2df4a3be8e777d3be792
mavibulut0106@gmail.com <mavibulut0106@gmail.com>
2023-04-02 22:37:18 +0300

    AOP

---
+++ Core/Utilities/Interceptors/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Castle.DynamicProxy;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, Castle.DynamicProxy.IInterceptor
+    {
+        public int Priority { get; set; }
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+
+        }
+    }
+
+}
